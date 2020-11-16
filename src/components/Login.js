39fcde32ea2c44b9779a3f570a098915cb2d617a
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2020-11-16 14:41:58 -0800

    added yup function and useEffect funtions for validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import * as yup from "yup";
 
@@ -7,6 +7,7 @@ export default function Login() {
   const [formState, setFormSate] = useState({
     username: "",
     password: "",
+    // accountType: "",
   });
 
   //submit state checks whether the form can be submited
@@ -17,9 +18,36 @@ export default function Login() {
 
   //onChange function
   const inputChange = (event) => {
+    console.log(event.target.name);
     console.log(event.target.value);
-    setFormSate(event.target.value);
+
+    const newFormState = {
+      ...formState,
+      [event.target.name]: event.target.value,
+    };
+    setFormSate(newFormState);
   };
+
+  //form schema set of rules
+  //object is coming from yup library
+  //shape function takes in an object {}
+  const formSchema = yup.object().shape({
+    username: yup.string().required("Username is required"),
+    password: yup.string().required("Password is required"),
+    // accountType: yup.string().oneOf(["diner", "operator"]),
+  });
+
+  useEffect(() => {
+    //isValid comes from the yup library
+    //checking formSchema against formState
+    //comparing the keys and the values
+    //returns a promise
+    formSchema.isValid(formState).then((valid) => {
+      //we can check the process has been completed
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+  //do something every time formState changes
 
   //onSubmit function
   const formSubmit = (event) => {};
@@ -50,10 +78,25 @@ export default function Login() {
             onChange={inputChange}
           />
         </label>
+        {/* not sure if log in page needs to a section for either diner or operator */}
+        {/* <label htmlFor="accountType">
+          Diner or Operator
+          <select
+            name="accountType"
+            id="accountType"
+            value={formState.accountType}
+            onChange={inputChange}
+          >
+            <option value="">Select</option>
+            <option value="diner">Diner</option>
+            <option value="operator">Operator</option>
+          </select>
+        </label> */}
         <button type="submit" disabled={buttonDisabled}>
           Log In
         </button>
         <Link to="/signup">Not a member?</Link>
+        <Link to="/">Home Page</Link>
       </form>
     </div>
   );
