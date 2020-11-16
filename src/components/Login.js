21d6561f3f5794e8210e6c21dc530b09ddf37d2d
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2020-11-16 09:47:43 -0800

    set up my form layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <p>Log In</p>
+      <form>
+        <label htmlFor="email">
+          Email
+          <input id="email" type="email" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input id="password" type="password" />
+        </label>
+        <button>Log In</button>
+        <Link to="/signup">Not a member?</Link>
+      </form>
+    </div>
+  );
 }
