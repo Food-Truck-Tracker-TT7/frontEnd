82b100cc23e8fb732b7aaae0e0d076f0f2f7ca46
Brian Reisman <brianreisman@protonmail.com>
2020-11-16 15:40:13 -0500

    yup validation working and errors populating in the DOM

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,3 @@
-//* create state for error messages
 //* populate informative error message
 //* set up yup validation and type to submitBTN
 
@@ -9,25 +8,44 @@ export default function Signup() {
   const initialState = { username: '', password: '', email: '', usertype: ''};
   const [signupInput, setSignupInput] = useState(initialState);
   const [validationError, setValidationError] = useState(initialState);
+  console.log(validationError);
   const [submitBtn, setSubmitBtn] = useState(false);
   console.log(signupInput);
 
   const onChangeFunc = (e) => {
     const typeOfValue = e.target.type === 'radio' ? e.target.id : e.target.value
     setSignupInput({ ...signupInput, [e.target.name]: typeOfValue });
+    validate(e);
   };
-
+  
   const onSubmitFunc = (e) => {
     e.preventDefault();
     setSignupInput(initialState);
   }
 
+  const validate = (e) => {
+    const typeOfValue = e.target.type === 'radio' ? e.target.checked : e.target.value //!I think this is e.target.checked for truthy
+    yup
+      .reach(formSchema, e.target.name) //reach into the formSchema object template and look for the e.target.name
+      .validate(typeOfValue) //...and validate typeOfValue against fomrSchema
+      .then( ifValid => {
+        setValidationError({...validationError, [e.target.name]: ''
+      })
+      })
+      .catch( ifErr => {
+        setValidationError({
+          ...validationError, [e.target.name]: ifErr.errors[0]
+        })
+      })
+
+  }
+
   //*Yup validation
   const formSchema = yup.object().shape({
-    username: yup.string().required(),
-    password: yup.string().required(),
-    email: yup.string().email().required(),
-    usertype: yup.boolean().oneOf([true]).required()
+    username: yup.string().required('This is not some fly by night app. All who wish to pass must supply a username'),
+    password: yup.string().required('We promise to keep your password a secret, but you do need to supply one'),
+    email: yup.string().email('Not so fast trickster! I know an email when I see one and this ain\'t it. Try again hot shot!').required('Who doesn\'t have an email address?'),
+    usertype: yup.boolean().oneOf([true]).required('You will NEVER see this message! Mwah hahaha!') //!This might be buggy one of the group of radio buttons needs to be true.
   });
 
   return (
@@ -49,7 +67,7 @@ export default function Signup() {
             name="username"
             value={signupInput.username}
             onChange={onChangeFunc}
-          />
+          /> {validationError.username}
         </label>
         <br />
         <label htmlFor="password">
@@ -60,7 +78,7 @@ export default function Signup() {
             name="password"
             value={signupInput.password}
             onChange={onChangeFunc}
-          />
+          /> {validationError.password}
         </label>
         <br />
         <label htmlFor="email">
@@ -71,7 +89,7 @@ export default function Signup() {
             name="email"
             value={signupInput.email}
             onChange={onChangeFunc}
-          />{" "}
+          />{validationError.email}
         </label>
         <br/>
         <button disabled={submitBtn}>Submit</button>
