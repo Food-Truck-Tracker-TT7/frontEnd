38952e6bf1441cde13e736b6bfc79afc91430480
Brian Reisman <brianreisman@protonmail.com>
2020-11-16 13:21:34 -0500

    initial commit. JSX built out. stated added for signup form values and submit button disabled

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Signup() {
-  return <div>Signup Page</div>;
+  const initialState = { username: '', password: '', email: '', usertype: ''};
+  const [signupInput, setSignupInput] = useState(initialState);
+  const [ submitBtn, setSubmitBtn] = useState(false);
+  console.log(signupInput); //!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  const onChangeFunc = (e) => {
+    setSignupInput({ ...signupInput, [e.target.name]: e.target.value });
+    console.log(e.target.name);
+    console.log(e.target.value);
+  };
+
+  const onSubmitFunc = (e) => {
+    e.preventDefault();
+    setSignupInput(initialState);
+  }
+
+  //TODO Validate w/Yup
+  return (
+    <>
+      <div>Signup Page</div>
+      <form onSubmit={onSubmitFunc}>
+        <label htmlFor="usertype"> Operator
+          <input type="radio" name='usertype' id='operator' value={signupInput.usertype} onChange={onChangeFunc}/>
+        </label>
+        <label htmlFor="usertype"> Diner
+          <input type="radio" name='usertype' id='diner' value={signupInput.usertype} onChange={onChangeFunc}/>
+        </label> 
+        <br />
+        <label htmlFor="username">
+          {"Username"}
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={signupInput.username} //*what is the value of this?<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            onChange={onChangeFunc}
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          {"Password"}
+          <input
+            type="text"
+            id="password"
+            name="password"
+            value={signupInput.password}
+            onChange={onChangeFunc}
+          />
+        </label>
+        <br />
+        <label htmlFor="email">
+          {"Email"}
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={signupInput.email}
+            onChange={onChangeFunc}
+          />{" "}
+        </label>
+        <br/>
+        <button disabled={submitBtn}>Submit</button>
+      </form>
+    </>
+  );
 }
