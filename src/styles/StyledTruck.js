904d19a38415fdd21ce06ec20c13d412f25ceb86
Brian Reisman <brianreisman@protonmail.com>
2020-11-18 13:52:38 -0500

    created style sheet for Truck component

---
+++ src/styles/StyledTruck.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const Truck = styled.div`
+  background-color: ${(props) => props.theme.backgroundColor};
+  color: ${(props) => props.theme.textColor};
+  height: 100vh;
+  box-sizing: border-box;
+  * {
+    width: 98vw;
+    /* border: 1px solid green; */
+  }
+  h2 { 
+    text-align: center;
+    font-size: 3.8rem;
+    margin-bottom: 1rem;
+  }
+  p {
+    text-align: center;
+  }
+  a {
+    text-decoration: underline dotted ${(props) => props.theme.accentColor};
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 2.4rem;
+`;
+
+export default Truck;
