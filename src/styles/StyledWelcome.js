14fde8e58db673e954f1fade09108075b8bc0e72
Brian Reisman <brianreisman@protonmail.com>
2020-11-18 17:11:36 -0500

    connected to theme colors

--- src/styles/StyledWelcome.js
+++ src/styles/StyledWelcome.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 const WelcomeStyle = styled.h2`
+font-size: 2.4rem;
+padding: 1rem;
+background:  ${(props) => props.theme.backgroundColor};
+color:  ${(props) => props.theme.textColor};
 
 `
 
